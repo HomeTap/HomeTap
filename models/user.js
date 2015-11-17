348bb71caa47983cf1c6659c2168cd231df4f24f
Peter Madsen <cephas.ryle@gmail.com>
2015-11-17 10:29:52 -0800

    add user db schema

---
+++ models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var accountSchema = new Schema({
+  username: String,
+  screenName: String,
+  isAdmin: Boolean,
+  queue: Array,
+  favorites: Array,
+  subscription: String
+});
+
+
+var User = mongoose.model('User', accountSchema);
+
+module.exports = User;

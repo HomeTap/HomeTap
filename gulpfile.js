02a4b61be22f9f7db241a9dc797bc2c35e9c580c
Peter Madsen <cephas.ryle@gmail.com>
2015-11-16 17:04:29 -0800

    update linting and remove notification section of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,27 @@
 'use strict';
 
 var gulp = require('gulp'),
-    notify = require('gulp-notify'),
     jshint = require('gulp-jshint'),
     jscs = require('gulp-jscs'),
     mocha = require('gulp-mocha');
 
-var jsPaths = ['./**/*.js'];
+var jsPaths = ['models/*.js', './*.js'];
 
-gulp.task('jsStyle', function () {
+gulp.task('jsStyle', function() {
   return gulp.src(jsPaths)
     .pipe(jscs())
-    .pipe(notify({
-      title: 'JSCS',
-      message: 'JSCS Passed'
-    }));
+    .pipe(jscs.reporter());
 });
 
-gulp.task('jsLint', function () {
+gulp.task('jsLint', function() {
   return gulp.src(jsPaths)
   .pipe(jshint())
-  .pipe(jshint.reporter('jshint-stylish'))
-  .pipe(jshint.reporter('fail'))
-  .pipe(notify({
-    title: 'JSHint',
-    message: 'JSHint Passed'
-  }));
+  .pipe(jshint.reporter('default'))
+  .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('test', function () {
+gulp.task('test', function() {
   return gulp.src('test/test.js', { read: false }).pipe(mocha());
 });
 
 gulp.task('default', ['jsLint', 'jsStyle']);
-// gulp.watch('src/*.js', ['default']);

7b256bdc90f257b998d7162495f6a8749b2a9a08
Peter Madsen <cephas.ryle@gmail.com>
2015-11-17 16:57:53 -0800

    add code for authentication testing

--- test/authentication_test.js
+++ test/authentication_test.js
@@ -2,7 +2,7 @@ var chai = require('chai'),
     chaiHttp = require('chai-http');
 
 var expect = chai.expect;
-var baseURL = 'http://localhost:3000/admin';
+var baseURL = 'http://localhost:3000';
 
 require('../app.js');
 chai.use(chaiHttp);
@@ -10,7 +10,7 @@ chai.use(chaiHttp);
 describe('Test Authentication', function() {
   it('Test No Authentication', function(done) {
     chai.request(baseURL)
-      .get('admin/beers')
+      .get('/admin/beers')
       .end(function(err, res){
         expect(res.status).to.equal(401);
         done();
@@ -18,22 +18,36 @@ describe('Test Authentication', function() {
   });
   
   it('Test Admin Authentication', function(done) {
-    chai.request(baseURL)
-      .get('admin/beers')
-      .auth('admin', 'password')
-      .end(function(err, res) {
-        expect(res.status).to.equal(200);
+    var admin = chai.request.agent(baseURL);
+
+    admin.post('/login')
+      .send({username: 'admin', password: 'password'})
+      .then(function(res) {
+        console.log(res);
+        expect(res).to.have.cookie('sessionid');
+        done()
+        // return admin.get('/admin/beers')
+        //   .then(function(res) {
+        //     expect(res).to.have.status(200);
+        //     done();
+        //   })
+        //   .catch(function(err) {
+        //     throw err;
+        //   });
+      })
+      .catch(function(err) {
+        throw err;
       });
   });
 
-  it('Test User Authentication', function() {
-    chai.request(baseURL)
-      .get('user/beers')
-      .auth('user', 'password')
-      .end(function(err, res) {
-        expect(res.status).to.equal(200);
-      });
-  });
+  // it('Test User Authentication', function() {
+  //   chai.request(baseURL)
+  //     .get('/user')
+  //     .auth('user', 'password')
+  //     .end(function(err, res) {
+  //       expect(res.status).to.equal(200);
+  //     });
+  // });
 
 
 });

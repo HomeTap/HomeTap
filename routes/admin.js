074b2ac0a748bfc62be16b494490f2de994986fe
Peter Madsen <cephas.ryle@gmail.com>
2015-11-17 16:57:04 -0800

    fix missing semicolon

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/beers', function(req, res) {
+  res.send(200, 'Beers for everyone');
+});
+
+router.post('/beers', function(req, res) {
+  var input = req.body;
+  res.status(200).send(input);
+  // input.save();
+});
+
+router.put('/beers', function(req,res) {
+
+});
+
 module.exports = router;

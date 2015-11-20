bf0647f9ac50d06c44b9f10a5c3196c211a0976d
Peter Madsen <cephas.ryle@gmail.com>
2015-11-20 01:39:21 -0800

    update user templates with bootstrap

--- public/javascript/user_home.js
+++ public/javascript/user_home.js
@@ -3,7 +3,12 @@ $(function() {
     $.ajax('/user/' + this.dataset.id, {
       method: 'PUT'
     }).done(function() {
-
     });
   });
+
+  $('#favorites').click(function() {
+    $(this).addClass('active');
+  });
 });
+
+

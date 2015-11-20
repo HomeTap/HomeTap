df6838df552d8b3c72ec9d04ffdad434fea562ae
Jeff Dunn <jdunn522@gmail.com>
2015-11-19 16:28:14 -0800

    Add buttons for favorite and queue changes

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var Schema = require('mongoose').Schema;
+var ObjectId = Schema.Types.ObjectId;
 
 var User = require('../models/user');
 var Category = require('../models/category');
@@ -42,72 +44,64 @@ router.get('/', function (req, res) {
   });
 });
 
-router.get('/beers', function (req, res){
+function renderLibrary(req, res, def) {
   User.findOne({userIdString: req.user._id}).lean().exec(function(error, result) {
     var favs  = result.favorites;
     var categories = {};
 
     Category.find({}, function(error, results){
-      var firstCategoryId = results[0]._id;
+      var category = results[0]._id;
+      if (!def) category = req.params.id;
+
       results.forEach(function(element) {
         categories[element._doc._id.toString()] = element._doc.category;
       });
 
       if(error) throw error;
-      Beer.find({categoryId: firstCategoryId}).lean().exec(function(error, beers) {
+      Beer.find({categoryId: category}).lean().exec(function(error, beers) {
         var newBeers = beers.map(function(beer) {
           beer.favorite = favs._id;
           return beer;
         });
 
         if(error) throw error;
-        console.log(categories);
         res.render('user_lib', {categorylist: categories, beerlist: newBeers});
       });
     });
   });
+}
+
+router.get('/beers', function (req, res){
+  renderLibrary(req, res, true);
 });
 
 router.get('/beers/category/:id', function(req, res) {
-  User.findOne({userIdString: req.user._id}).lean().exec(function(error, result) {
-    var favs  = result.favorites;
-    var categories = {};
-
-    Category.find({}, function(error, results){
-      results.forEach(function(element) {
-        categories[element._doc._id.toString()] = element._doc.category;
-      });
-
-      if(error) throw error;
-      Beer.find({categoryId: req.params.id}).lean().exec(function(error, beers) {
-        var newBeers = beers.map(function(beer) {
-          beer.favorite = favs._id;
-          return beer;
-        });
-
-        if(error) throw error;
-        console.log(categories);
-        res.render('user_lib', {categorylist: categories, beerlist: newBeers});
-      });
-    });
-  });
+  renderLibrary(req, res, false);
 });
 
 router.put('/beers/favorite/:id', function(req, res) {
-  var categories = {};
-
-  Category.find({}, function(error, results){
-    results.forEach(function(element) {
-      categories[element._doc._id.toString()] = element._doc.category;
-    });
-
-    if(error) throw error;
-    Beer.find({categoryId: req.params.id}, function(error, beers) {
-      if(error) throw error;
-      console.log(categories);
-      res.render('user_lib', {categorylist: categories, beerlist: beers});
-    });
+  User.find({userIdString: req.user._id}, function(error, results) {
+    console.log('user: ', results);
+    var favs = results.favorites;
+    if (req.params.id in favs) {
+      favs = favs.splice(indexOf(req.params.id), 1);
+      User.update({userIdString: req.user._id}, {$set: {favorites: favs}}, function(error, results) {
+        if (error) throw error;
+        renderLibrary(req, res, true);
+      });
+    } else {
+      favs.push(ObjectId(req.params.id))
+      User.update({userIdString: req.user._id}, {$set: {favorites: favs}}, function(error, results) {
+        if (error) throw error;
+        renderLibrary(req, res, true);
+      });
+    }
   });
+});
+
+router.put('/beers/queue/:id', function(req, res) {
+  // adjusting user's queue
+  renderLibrary(req, res, true); // adjust so that we render a view for the currently selected category
 });
 
 module.exports = router;

6ecf208360568837d16b3ab2a9cbb635729fc1d5
Jeff Dunn <jdunn522@gmail.com>
2015-11-22 19:18:52 -0800

    Ensure consistent code style in 'test' js files

--- test/admin_router_test.js
+++ test/admin_router_test.js
@@ -1,22 +1,19 @@
-var chai = require('chai'),
-    chaiHttp = require('chai-http');
-
+var chai = require('chai');
+var chaiHttp = require('chai-http');
 var expect = chai.expect;
+
 var baseURL = 'http://localhost:3000/admin';
 
 require('../app.js');
 chai.use(chaiHttp);
 
 describe('Test admin routes', function() {
-
   describe('Test beer library routes', function() {
-
     it('should get a response', function(done) {
       chai.request(baseURL)
         .get('/beers')
-        .end(function(err, res) {
+        .end(function(err) {
           expect(err).to.be.an('null');
-          console.log(res);
           done();
         });
     });
@@ -26,17 +23,14 @@ describe('Test admin routes', function() {
         .post('/beers')
         .send({
           name: 'Tasty',
-          categoryId: 17,
+          categoryId: '564c34c49ce07853eb58086d',
           description: 'real good stuff',
           stars: 4.2
         })
-        .end(function(err, res) {
+        .end(function(err) {
           expect(err).to.be.an('null');
-          console.log(res);
           done();
         });
     });
-
   });
-
 });
